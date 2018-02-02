b055a34071972b0094ad8bf6d0375a2696643972
heheyuanqing <2570394541@qq.com>
2018-02-02 19:48:57 +0800

    todolist(4)

--- todolist/src/thing.js
+++ todolist/src/thing.js
@@ -1,16 +1,29 @@
 import React,{Component} from 'react';
 
 class Thing extends Component{
+   constructor(){
+       super();
+       this.state={alreadyClick:false}
+   }
+
     handleDelete(){
         if(this.props.onDeleteThing){
             this.props.onDeleteThing(this.props.index);
         }
     }
+    changeSpan(){
+        this.setState({
+            alreadyClick:!this.state.alreadyClick
+        });
+        this.state.alreadyClick?this.span.className="":this.span.className="textStyle";
+
+    }
     render(){
+
         return(
             <div className="thing">
-                <input type="radio"/>
-                <span>{this.props.thing.thing}</span>
+                <input type="checkbox" onClick={this.changeSpan.bind(this)} />
+                <span ref={(span)=>this.span=span} >{this.props.thing.thing}</span>
                 <button className="delete" onClick={this.handleDelete.bind(this)}>X</button>
             </div>
         )
